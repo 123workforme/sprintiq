d50fd100086c061b1f8b87f307bc75c9b2d88352
Michael <notthefederallaw@gmail.com>
2026-06-18 18:26:26 -0400

    install libgles2 system dep in build.sh to fix mediapipe on render

--- backend/build.sh
+++ backend/build.sh
@@ -1,3 +1,4 @@
+sudo apt-get update -y && sudo apt-get install -y libgles2 libgl1 libegl1 libglib2.0-0 || true
 pip install -r requirements.txt
 pip uninstall -y opencv-python opencv-contrib-python || true
 pip install --force-reinstall opencv-python-headless==4.13.0.92
